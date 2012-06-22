480ecacc4150465f2447aab3137bf5bf20bbc9c1
Filip Pyda <filip.pyda@gmail.com>
2012-06-22 08:45:26 +0200

    Set session timeout to 100 years

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,4 +4,4 @@
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-Ladder8::Application.config.session_store :active_record_store
+Ladder8::Application.config.session_store :active_record_store, :expire_after => 100.years
